d12926e38dbc276de92ad411fe7d755861ccf2e1
Valeria Avila <valeria.avila17@gmail.com>
2020-08-23 10:56:24 -0700

    Set up DbConrtext and product class

---
+++ eCommerceSite/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    public class Product
+    {
+        [Key] // Make Primary Key in database
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// The consumer facing name of the product
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The retail price as US currency
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Category product falls under. Ex. Electronics, Furniture, etc
+        /// </summary>
+        public string Category { get; set; }
+    
+    }
+}
