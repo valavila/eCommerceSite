8da2f6a7fe7c99df380b8a95ff41eb78208796ad
Valeria Avila <valeria.avila17@gmail.com>
2020-08-23 18:16:27 -0700

    Created product listing page. Added a link to the product catalog in the nav bar

---
+++ eCommerceSite/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceSite.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductContext _context;
+        public ProductController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Displays a view that lists all products
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            //Get sll producrs from databse
+            //List<Product> products = _context.Products.ToList();
+            List<Product> products = (from p in _context.Products
+                                      select p).ToList();
+
+            //Send list of products to view to be diplayed
+            return View(products);
+        }
+    }
+}
