9ab008947c4325bbda5d1ad9ed68dca6410caa26
valavila <64001419+valavila@users.noreply.github.com>
2020-08-24 08:16:19 -0700

    Issue#3 add product (#11)
    * Adding product is implemented
    
    * Styled Product added successfully message
    
    * Refactored DB coed to run asynchronlusly

--- eCommerceSite/Controllers/ProductController.cs
+++ eCommerceSite/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using eCommerceSite.Data;
 using eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerceSite.Controllers
 {
@@ -20,15 +21,39 @@ namespace eCommerceSite.Controllers
         /// Displays a view that lists all products
         /// </summary>
         /// <returns></returns>
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             //Get sll producrs from databse
             //List<Product> products = _context.Products.ToList();
-            List<Product> products = (from p in _context.Products
-                                      select p).ToList();
+            List<Product> products = await (from p in _context.Products
+                                      select p).ToListAsync();
 
             //Send list of products to view to be diplayed
             return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                //Add to DB
+                _context.Products.Add(p); // Creates insert querey
+                await _context.SaveChangesAsync(); // executes query
+
+                TempData["Message"] = $"{p.Title} was added successfully"; // populate success message
+
+                //Rederict back to catalog page
+                return RedirectToAction("Index");
+            }
+
+            return View();
         }
     }
 }
