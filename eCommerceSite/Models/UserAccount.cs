d2c0329955c0787f5f85a365bc017fdeabf15f84
Valeria Avila <valeria.avila17@gmail.com>
2020-08-27 05:17:13 -0700

    Added register page and mofe validation

--- eCommerceSite/Models/UserAccount.cs
+++ eCommerceSite/Models/UserAccount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -24,19 +25,23 @@ namespace eCommerceSite.Models
     public class RegisterViewModel
     {
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
         [Compare(nameof(Email))]
         [Required]
-        public string  ConfimeEmail { get; set; }
+        [Display(Name = "Confirm Email")]
+        public string  ConfirmEmail { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [StringLength(120, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 120 characters ")]
         public string Password { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
         [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
         public string ConfirmPassword { get; set; }
 
         [DataType(DataType.Date)] // Time is ignored
