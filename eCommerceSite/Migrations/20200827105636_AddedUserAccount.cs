c116293a243c94e931224cfac71a613cff598bf1
valavila <64001419+valavila@users.noreply.github.com>
2020-08-27 05:40:19 -0700

    Issue#20 registration (#23)
    * Created a user account model
    
    * Added user account to databse using code first migration
    
    * Create Register View Model
    
    * Added register page and mofe validation
    
    * Add registered user into DB

---
+++ eCommerceSite/Migrations/20200827105636_AddedUserAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eCommerceSite.Migrations
+{
+    public partial class AddedUserAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserAccounts",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(nullable: true),
+                    UserName = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    DateOfBirth = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAccounts", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAccounts");
+        }
+    }
+}
---
+++ eCommerceSite/Models/UserAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Compare(nameof(Email))]
+        [Required]
+        [Display(Name = "Confirm Email")]
+        public string  ConfirmEmail { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(120, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 120 characters ")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [DataType(DataType.Date)] // Time is ignored
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
