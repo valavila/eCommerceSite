7f208c61ae5dbaf7f3a5652c3b1df71edefcf639
Valeria Avila <valeria.avila17@gmail.com>
2020-09-01 21:16:21 -0700

    Added log out

--- eCommerceSite/Controllers/UserController.cs
+++ eCommerceSite/Controllers/UserController.cs
@@ -86,5 +86,13 @@ namespace eCommerceSite.Controllers
 
             return RedirectToAction("Index", "Home");
         }
+
+        public IActionResult Logout()
+        {
+            // Removes all current session data
+            HttpContext.Session.Clear();
+
+            return RedirectToAction(actionName: "Index", controllerName: "Home");
+        }
     }
 }
