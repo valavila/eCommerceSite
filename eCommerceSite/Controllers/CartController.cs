4be3b8c3ae70059fb832ecd964b3b096ce485fbc
Valeria Avila <valeria.avila17@gmail.com>
2020-09-01 22:18:13 -0700

    Refactored cart controller

--- eCommerceSite/Controllers/CartController.cs
+++ eCommerceSite/Controllers/CartController.cs
@@ -30,33 +30,7 @@ namespace eCommerceSite.Controllers
         {
             Product p = await ProductDb.GetSingleProductAsync(_context, id);
 
-            const string CartCookie = "CartCookie";
-
-            // Get existing cart items
-            string existingItems =
-                _httpContext.HttpContext.Request.Cookies[CartCookie];
-
-            List<Product> cartProducts = new List<Product>();
-            if (existingItems != null)
-            {
-                cartProducts = JsonConvert.DeserializeObject<List<Product>>(existingItems);
-            }
-
-            // Add current product to existing cart
-            cartProducts.Add(p);
-
-            // Add product list to cart cookie
-            string data = JsonConvert.SerializeObject(cartProducts);
-
-            
-            CookieOptions options = new CookieOptions()
-            {
-                Expires = DateTime.Now.AddYears(1),
-                Secure = true,
-                IsEssential = true
-            };
-
-            _httpContext.HttpContext.Response.Cookies.Append(CartCookie, data, options);
+            CookieHelper.AddProductToCart(_httpContext, p);
 
             // Redirect back to previous page
             return RedirectToAction("Index", "Product");
@@ -64,12 +38,7 @@ namespace eCommerceSite.Controllers
 
         public IActionResult Summary()
         {
-            // Display all products in shopping cart cookie
-            string cookieData = _httpContext.HttpContext.Request.Cookies["CartCookie"];
-
-            List<Product> cartProducts =
-                JsonConvert.DeserializeObject<List<Product>>(cookieData);
-
+            List<Product> cartProducts = CookieHelper.GetCartProducts(_httpContext);
             return View(cartProducts);
         }
     }
---
+++ eCommerceSite/Models/CookieHelper.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    public static class CookieHelper
+    {
+        const string CartCookie = "CartCookie";
+
+        /// <summary>
+        /// Returns the current list of cart products. If cart is empty
+        /// an empty list will be returned
+        /// </summary>
+        /// <param name="http"></param>
+        /// <returns>An empty list if cart is empty</returns>
+        public static List<Product> GetCartProducts(IHttpContextAccessor http)
+        {
+            // Get existing cart items
+            string existingItems = http.HttpContext.Request.Cookies[CartCookie];
+
+            List<Product> cartProducts = new List<Product>();
+            if (existingItems != null)
+            {
+                cartProducts = JsonConvert.DeserializeObject<List<Product>>(existingItems);
+            }
+
+            return cartProducts;
+        }
+
+        public static void AddProductToCart(IHttpContextAccessor http, Product p)
+        {
+            List<Product> cartProducts = GetCartProducts(http);
+            cartProducts.Add(p);
+
+            string data = JsonConvert.SerializeObject(cartProducts);
+
+            CookieOptions options = new CookieOptions()
+            {
+                Expires = DateTime.Now.AddYears(1),
+                Secure = true,
+                IsEssential = true
+            };
+
+            http.HttpContext.Response.Cookies.Append(CartCookie, data, options);
+        }
+
+        public static int GetTotalCartProducts(IHttpContextAccessor http)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
