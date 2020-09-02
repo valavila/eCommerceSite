a01349fa973de813d24fc0f7e84be7985e9aa53c
valavila <64001419+valavila@users.noreply.github.com>
2020-09-01 22:28:10 -0700

    Issue#22 shopping cart (#35)
    * changed nav text color for login, register and welcome
    
    * Added shopping cart link to navigation and add to cart button for each product
    
    * An item can now be added to cart
    
    * Able to store multiple items in shopping cart
    
    * Created basic summary page. Does not work with empty cookie
    
    * Refactored cart controller
    
    * Added a message for an empty cart on the summary page

---
+++ eCommerceSite/Controllers/CartController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace eCommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ProductContext _context;
+        private readonly IHttpContextAccessor _httpContext;
+
+        public CartController(ProductContext context, IHttpContextAccessor httpContext)
+        {
+            _context = context;
+            _httpContext = httpContext;
+        }
+
+        /// <summary>
+        /// Adds a product to the shopping cart
+        /// </summary>
+        /// <param name="id">The id of the product to add</param>
+        /// <returns></returns>
+        public async Task<IActionResult> Add(int id)
+        {
+            Product p = await ProductDb.GetSingleProductAsync(_context, id);
+
+            CookieHelper.AddProductToCart(_httpContext, p);
+
+            // Redirect back to previous page
+            return RedirectToAction("Index", "Product");
+        }
+
+        public IActionResult Summary()
+        {
+            List<Product> cartProducts = CookieHelper.GetCartProducts(_httpContext);
+            return View(cartProducts);
+        }
+    }
+}
